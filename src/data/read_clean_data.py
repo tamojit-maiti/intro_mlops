fb557298bea43295fcb52b1975e4e8783ac1a537
tamojit-maiti <maiti.tamojit@gmail.com>
2022-09-19 15:36:37 +0530

    added mae function

--- src/data/read_clean_data.py
+++ src/data/read_clean_data.py
@@ -2,6 +2,9 @@ import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt 
 
+def read_raw_data():
+    pass
+
 def mape(y_true: np.array,
         y_pred: np.array):
     """
@@ -9,3 +12,12 @@ def mape(y_true: np.array,
     
     """
     return 100 * np.mean(np.abs(y_true - y_pred)/y_true)
+
+def mae(y_true: np.array,
+        y_pred: np.array):
+    """
+    Calculates the Mean Absolute Error of y_pred with respect to y_true
+    
+    """
+    assert np.shape(y_pred) == np.shape(y_true), 'the shapes of the two arrays must be the same'
+    return np.mean(np.abs(y_true - y_pred))
